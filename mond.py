b6bac352dbd878f893a9617763f3add4f9a69ef3
Feiyi Wang <fwang2@gmail.com>
2015-01-23 14:59:13 -0500

    update logging

--- mond.py
+++ mond.py
@@ -11,8 +11,9 @@ import zmq
 import glob
 import imp
 
-logging.basicConfig(level=logging.DEBUG,
-        format="%(asctime)s - %(levelname)s\t - %(message)s")
+# Globals
+logger  = None
+ARGS    = None
 
 class G:
 
@@ -20,42 +21,70 @@ class G:
     socket = None
     config = None
     plugins = {}
-    callback = 'get_stats'
+    callbacks = ['metric_init', 'get_stats', 'metric_cleanup']
+
+
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="MOND program")
+    parser.add_argument("-v", "--verbose", default=False, action="store_true", help="verbose output")
+    myargs = parser.parse_args()
+    return myargs
 
 def zmq_init(pub):
 
-    logging.debug("Bind to %s" % pub)
+    logger.debug("Bind to %s" % pub)
     G.context = zmq.Context()
     G.socket = G.context.socket(zmq.PUB)
     G.socket.bind(pub)
 
 def sig_handler(signal, frame):
 
-    print "\tUser cancelled!"
+    print "\tUser cancelled ... cleaning up"
+    plugin_cleanup()
     sys.exit(0)
 
 
-def plugin_init(pathname):
+def plugin_scan(pathname):
 
     pathname = os.path.realpath(pathname) + "/*.py"
-
-    logging.debug("Plugin path: %s" % pathname)
-
+    logger.debug("Plugin path: %s" % pathname)
     sources = glob.glob(pathname)
-
 
     for s in sources:
         name = os.path.basename(s).split(".")[0]
         mod = imp.load_source(name, s)
-        if G.callback in dir(mod):
+        if set(G.callbacks).issubset(set(dir(mod))):
             G.plugins[name] = mod
-            logging.info("Registering plugin: %s" % name)
+            logger.info("Registering plugin: %s" % name)
         else:
-            logging.warn("Skipping %s" % name)
+            logger.warn("Skipping %s" % name)
+
+def plugin_init():
+    for name, mod in G.plugins.iteritems():
+        mod.metric_init(name)
+
+def plugin_cleanup():
+    for name, mod in G.plugins.iteritems():
+        mod.metric_cleanup()
+
 
 def main():
+    global logger, ARGS
 
     signal.signal(signal.SIGINT, sig_handler)
+
+    logger = logging.getLogger("main")
+
+    ARGS = parse_args()
+    if ARGS.verbose:
+        logging.basicConfig(level=logging.DEBUG,
+            format="%(asctime)s - %(name)s - %(levelname)s\t - %(message)s")
+    else:
+        logging.basicConfig(level=logging.INFO,
+            format="%(name)s - %(message)s")
+
 
     G.config = ConfigParser.SafeConfigParser()
     G.config.read("oddman.conf")
@@ -65,15 +94,18 @@ def main():
 
     # initialize all metric modules
 
-    plugin_init("./plugins")
+    plugin_scan("./plugins")
+    plugin_init()
 
     while True:
         for name, mod in G.plugins.iteritems():
             msg = mod.get_stats()
-            logging.debug("%s -> %s" % (name, msg))
+            logger.debug("%s -> %s" % (name, msg))
             G.socket.send_string(msg)
 
         time.sleep(interval)
+
+    plugin_cleanup()
 
 if __name__ == "__main__": main()
 
--- plugins/metric_ost_stats.py
+++ plugins/metric_ost_stats.py
@@ -1,5 +1,85 @@
+#!/usr/bin/env python
+
+import os
+import glob
+import logging
+import json
+import time
+from collections import defaultdict
+
+# Globals
+
+logger = None
+
+class G:
+    fsname = None
+    ostnames = None
+    stats = defaultdict(lambda: defaultdict(int))
+
+def scan_osts():
+    osts = glob.glob("/proc/fs/lustre/obdfilter/*")
+    ostnames = []
+    if len(osts) != 0:
+        fsname, _ = os.path.basename(osts[0]).split("-")
+        for ost in osts:
+            ostnames.append(os.path.basename(ost))
+    else:
+        logger.error("Can't locate Lustre OSTs")
+
+    return fsname, ostnames
 
 
+def read_ost_stats(f):
+    """
+    expect input of a path to ost stats
+    return a dictionary with key/val pairs
+    """
+    ret = {'read_bytes_sum': 0, 'write_bytes_sum': 0}
+
+    pfile = os.path.normpath(f) + "/stats"
+    with open(pfile, "r") as f:
+            for line in f:
+                chopped = line.split()
+                if chopped[0] == "snapshot_time":
+                    ret["snapshot_time"] = chopped[1]
+                if chopped[0] == "write_bytes":
+                    ret["write_bytes_sum"] = chopped[6]
+                if chopped[0] == "read_bytes":
+                    ret["read_bytes_sum"] = chopped[6]
+
+    return ret
+
+
+def update():
+    logger.debug("Updating stats")
+
+    for ost in G.ostnames:
+        fpath = '/proc/fs/lustre/obdfilter/' + ost
+        G.stats[ost] = read_ost_stats(fpath)
+
+    logger.debug("Sucessfully refreshing OST stats")
+
+
+def metric_init(name, loglevel=logging.DEBUG):
+    global logger
+    logging.basicConfig(level=loglevel,
+                    format="%(asctime)s - %(name)s - %(levelname)s\t - %(message)s")
+    logger = logging.getLogger(name)
+
+    G.fsname, G.ostnames = scan_osts()
 
 def get_stats():
-    return "osd: get stats"
+    update()
+
+    return json.dumps(G.stats)
+
+def metric_cleanup():
+    pass
+
+if __name__ == '__main__':
+    metric_init("ost-stats")
+    while True:
+        print get_stats()
+        time.sleep(5)
+    metric_cleanup()
+
