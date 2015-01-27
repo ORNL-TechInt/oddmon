a3780e52251b076601d38c5919d8d42b98bcb7de
Feiyi Wang <fwang2@gmail.com>
2015-01-27 07:09:17 -0500

    add dependecies and missing spec options

--- setup.py
+++ setup.py
@@ -1,9 +1,23 @@
 from distutils.core import setup
 import glob
+import sys
 
 plugins = glob.glob("metric*.py")
 
+requires = [
+        'setuptools',
+        'argparse']
+if sys.version_info[:3] < (2,5,0):
+    raise RuntimeError("This application requires Python 2.6+")
+
+details="""
+More details on the package
+"""
+
 setup(name='oddmon',
+      description="A distributed monitoring tool suite",
+      url="http://github.com/fwang2/oddmon",
+      license="LGPL",
       version='0.1',
       author='Feiyi Wang',
       author_email='fwang2@ornl.gov',
@@ -13,7 +27,8 @@ setup(name='oddmon',
                   ('/etc/oddmon', ['oddmon.conf']),
                   ('/usr/share/doc/oddmon', ['README.md'])
                   ],
-      requires=['argparse']
+      install_requires=requires,
+      long_description=details
       )
 
 
