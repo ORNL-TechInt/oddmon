58dc0ab42f273917b8feacafa15d97db0193dded
Feiyi Wang <fwang2@gmail.com>
2015-01-29 02:01:10 -0500

    pkg changes

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from distutils.core import setup
 import glob
 import sys
-
 plugins = glob.glob("metric*.py")
 
 requires = [
-        'setuptools',
+        'pyzmq',
         'argparse']
 if sys.version_info[:3] < (2,5,0):
     raise RuntimeError("This application requires Python 2.6+")
@@ -15,20 +14,28 @@ More details on the package
 """
 
 setup(name='oddmon',
-      description="A distributed monitoring tool suite",
-      url="http://github.com/fwang2/oddmon",
-      license="LGPL",
-      version='0.1',
-      author='Feiyi Wang',
-      author_email='fwang2@ornl.gov',
-      py_modules=['hostlist', 'lfs_utils'],
-      scripts=['odd_server.py','odd_client.py'],
-      data_files=[ ('/usr/lib64/oddmon', plugins),
-                  ('/etc/oddmon', ['oddmon.conf']),
-                  ('/usr/share/doc/oddmon', ['README.md'])
+    description="A distributed monitoring tool suite",
+    url="http://github.com/fwang2/oddmon",
+    license="LGPL",
+    version='0.1a1',
+    author='Feiyi Wang',
+    author_email='fwang2@ornl.gov',
+    py_modules=['hostlist', 'lfs_utils'],
+    scripts=['odd_server.py','odd_client.py'],
+    data_files=[ ('lib/oddmon', plugins),
+                  ('lib/oddmon', ['oddmon.cfg.sample']),
+                  ('share/doc/oddmon', ['README.md'])
                   ],
-      install_requires=requires,
-      long_description=details
+    classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: System Administrators',
+            'Topic :: System :: Monitoring',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+      ],
+    install_requires = requires,
+    long_description=details
       )
 
 
