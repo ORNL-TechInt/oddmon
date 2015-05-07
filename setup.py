66d3c48c3929c7fc705dd5012b3508fbf07d258d
Feiyi Wang <fwang2@gmail.com>
2015-05-06 22:32:25 -0400

    add make rpm

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ import setuptools
 
 import glob
 import sys
-plugins = glob.glob("metric*.py")
+
+# plugins = glob.glob("metric*.py")
 
 requires = [
         'pyzmq',
         'argparse']
-if sys.version_info[:3] < (2,5,0):
-    raise RuntimeError("This application requires Python 2.6+")
+if sys.version_info[:3] < (2,7,0):
+    raise RuntimeError("This application requires Python 2.7+")
 
 details="""
 More details on the package
@@ -16,22 +17,25 @@ More details on the package
 
 setuptools.setup(name='oddmon',
     description="A distributed monitoring tool suite",
-    url="http://github.com/fwang2/oddmon",
+    url="http://github.com/ORNL-TechInt/oddmon",
     license="LGPL",
-    version='0.1a1',
+    version='0.1',
     author='Feiyi Wang',
     author_email='fwang2@ornl.gov',
-    py_modules=['monctl', 'oddpub', 'oddsub','hostlist', 'lfs_utils','daemon'],
+    py_modules=['monctl', 'oddpub', 'oddsub','hostlist',
+                'lfs_utils', 'daemon',
+                'metric_ost_stats',
+                'metric_ost_brw_stats'],
     entry_points={
         'console_scripts': [
             'monctl=monctl:main'
         ]
     },
-    data_files=[ ('lib/oddmon', plugins),
-                  ('lib/oddmon', ['oddmon.cfg.sample']),
-                  ('share/doc/oddmon', ['README.md']),
-                ('/etc/init.d', ['collector', 'aggregator'])
-                  ],
+
+    #             data_files=[ ('lib/oddmon', ['oddmon.cfg.sample']),
+    #              ('share/doc/oddmon', ['README.md']),
+    #            ('lib/oddmon', ['collector', 'aggregator'])
+    #            ],
     classifiers=[
             'Development Status :: 3 - Alpha',
             'Intended Audience :: System Administrators',
