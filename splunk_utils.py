26a4082785de7ba3faf2cad46b1d1b0787efb943
Jeremy Anantharaj <anantharajjd@ornl.gov>
2015-05-21 10:52:24 -0400

    Enable reporting of the rest of the brw_stats

--- splunk_utils.py
+++ splunk_utils.py
@@ -27,20 +27,20 @@ def connect_to_splunk():
     # This section should get cut out once we've got the api_ddntools
     # user set up
     #SPLUNK_USER=getpass.getuser()
-    SPLUNK_USER="xmr"
+    SPLUNK_USER="j4y"
     SPLUNK_PWD=getpass.getpass( 'Password for user %s:'%SPLUNK_USER)
 
     G.service = client.connect( host=G.SPLUNK_HOST, port=G.SPLUNK_PORT,
-                              username=SPLUNK_USER, password=SPLUNK_PWD)
+            username=SPLUNK_USER, password=SPLUNK_PWD)
 
 
 def push_to_splunk( msg):
-    
+
     logger.debug("Calling push_to_splunk...")
     blob = json.loads(msg[0])
-    
+
     index = G.service.indexes[G.SPLUNK_INDEX]
-    
+
     print "-----------------------------"
     print "blob type: %s"%type(blob)
     print "blob keys: %s"%blob.keys()
@@ -55,28 +55,27 @@ def push_to_splunk( msg):
     print "type of first stats value: %s"%type(stats[stats.keys()[0]])
     print "first stats value keys: %s"%stats[stats.keys()[0]].keys()
     print "============================="
-        
+
     stats = ast.literal_eval(blob[u'metric_ost_brw_stats'])
     for ost in stats.keys():
         metrics_dict = stats[ost]
         snapshot_time = float(metrics_dict["snapshot_time"])
         for metric in metrics_dict.keys():
             # event_str = "snapshot_time = %f "%snapshot_time
-            
+
             # The values for the different metrics need to be handled differently
             if metric == "snapshot_time":
                 continue # snapshot_time is not a metric in and of itself
-            elif (metric == "discontinuous_blocks_read") or \
-                 (metric == "discontinuous_blocks_write"):
-                logger.debug("discontinuous_blocks_*")
+            else:
+                logger.debug(metric)
                 value = metrics_dict[metric]
                 for k in value.keys():
-                    event_str = "snapshot_time = %f bucket=%s counts=%s"%(snapshot_time, k, value[k][0])
+                    event_str = "snapshot_time = %f bucket=%s counts=%s" %(snapshot_time, k, value[k][0])
                     # logger.debug( "event_str: %s"%event_str)
                     # Commented out - too much text even for debug
                     index.submit( event_str, host = ost, source=metric, sourcetype="brw_stats")
-             
-    
+
+
 
 
 # Set up the logger
