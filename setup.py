0e7744b41612bab060cc1b914cc9a075a0653dd0
Ross Miller <rgmiller@ornl.gov>
2016-08-15 15:29:29 -0400

    Add support for RHEL7
    Changes to support RHEL 7: systctl start/stop scripts and changes to
    setup.py to detect distro version and include either the new sysctl
    scripts or the old upstart scripts.
    
    Also bumped the version to 1.5.0

--- setup.py
+++ setup.py
@@ -1,9 +1,30 @@
 import setuptools
-
-import glob
+import platform
 import sys
 
-# plugins = glob.glob("metric*.py")
+def get_distro():
+    '''
+    Returns a tuple consisting of the distro name (string) and major
+    number (int)
+    '''
+    distro, version = platform.dist()[0:2]
+    majorVersion = int(version.split('.')[0])
+    return (distro, majorVersion)
+
+# Do some sanity checks on the distribution we're packaging for
+(distro, version) = get_distro()
+if not (distro == "redhat" or
+        distro == "centos"):
+    raise RuntimeError("Unknown Linux distribution: '%s'. " % distro +
+                       "We can only build packages for RedHat Enterprise " +
+                       "Linux and CentOS.")
+
+if version < 6:
+    raise RuntimeError("Linux distribution too old.  We need at least v6.")
+
+if version > 7:
+    raise RuntimeError("Linux distribution too new.  v7 is the latest " +
+                       "currently supported.")
 
 if sys.version_info[:3] < (2,6,0):
     raise RuntimeError("This application requires Python 2.6+")
@@ -12,11 +33,20 @@ details="""
 More details on the package
 """
 
+# Choose the appropriate startup script to include in the package
+if version == 7:
+    # Use the systemd script
+    startup_tuple = ('/usr/lib/systemd/system', ['oddmon_aggregator.service','oddmon_collector.service'])
+else:
+    # Use the upstart script
+    startup_tuple = ('/etc/init', ['oddmon_aggregator.conf','oddmon_collector.conf'])
+
+
 setuptools.setup(name='oddmon',
     description="A distributed monitoring tool suite",
     url="http://github.com/ORNL-TechInt/oddmon",
     license="LGPL",
-    version='1.4.3',
+    version='1.5.0',
     author='Feiyi Wang, Ross Miller, Jeremy Anantharaj',
     author_email='fwang2@ornl.gov, rgmiller@ornl.gov, anantharajjd@ornl.gov',
     packages = ['oddmon', 'oddmon.metric_plugins'],
@@ -27,12 +57,13 @@ setuptools.setup(name='oddmon',
     scripts = ['monctl.py'],
     data_files=[ ('/etc/oddmon', ['oddmon.cfg.sample']),
                  ('share/doc/oddmon', ['README.md']),
-                 ('/etc/init', ['oddmon_aggregator.conf','oddmon_collector.conf'])
+                 startup_tuple
                ],
     classifiers=[
-            'Development Status :: 3 - Alpha',
+            'Development Status :: 5 - Production/Stable',
             'Intended Audience :: System Administrators',
             'Topic :: System :: Monitoring',
+            'Operating System :: POSIX :: Linux',
             'Programming Language :: Python :: 2',
             'Programming Language :: Python :: 2.6',
             'Programming Language :: Python :: 2.7',
