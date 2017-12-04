b89974700391a885b3e9846a2459b6ededb6ce5c
Ross Miller <rgmiller@ornl.gov>
2017-12-04 14:22:55 -0500

    Add GNU GPL statement to source files.  No actual code changes.

--- oddmon/lfs_utils.py
+++ oddmon/lfs_utils.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python
 
+# This work was supported by the Oak Ridge Leadership Computing Facility at
+# the Oak Ridge National Laboratory, which is managed by UT Battelle, LLC for
+# the U.S. DOE (under the contract No. DE-AC05-00OR22725).
+#
+#
+# This file is part of OddMon.
+#
+# OddMon is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 2 of the License, or (at your option) any later
+# version.
+#
+# OddMon is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along
+# with OddMon.  If not, see <http://www.gnu.org/licenses/>.
+
 import os
 import glob
 import logging
